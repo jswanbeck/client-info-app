b6190f69e58296cda08b4683b71d18f5ad57df45
Jimmy <jimmy.swanbeck@gmail.com>
2025-06-30 13:21:59 -0400

    display custom properties in client details panel

--- src/app/clients/components/client-info/client-info.component.ts
+++ src/app/clients/components/client-info/client-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Client } from '../../models/client.model';
 
 @Component({
@@ -6,9 +6,20 @@ import { Client } from '../../models/client.model';
   templateUrl: './client-info.component.html',
   styleUrls: ['./client-info.component.scss'],
 })
-export class ClientInfoComponent {
+export class ClientInfoComponent implements OnChanges {
   @Input() client!: Client;
   @Output() clientSelect = new EventEmitter<Client>();
+
+  customProperties: { key: string; value: string | number | undefined }[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['client'] && this.client) {
+      const baseClientProperties = ['id', 'name', 'title', 'avatar', 'quote', 'nationality'];
+      this.customProperties = Object.keys(this.client)
+        .filter(key => !baseClientProperties.includes(key))
+        .map(key => ({ key, value: this.client[key] }));
+    }
+  }
 
   onClick() {
     this.clientSelect.emit(this.client);
